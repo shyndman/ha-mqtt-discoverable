7988b3536f7bb3bb1cd84cd45838394264a4ecca
Scott Hyndman <scotty.hyndman@gmail.com>
2025-02-25 03:15:39 -0800

    Strip dashes off the end of "clean" strings

--- ha_mqtt_discoverable/utils.py
+++ ha_mqtt_discoverable/utils.py
@@ -24,8 +24,7 @@ def clean_string(raw: str) -> str:
     """
     MQTT Discovery protocol only allows [a-zA-Z0-9_-]
     """
-    result = re.sub(r"[^A-Za-z0-9_-]", "-", raw)
-    return result
+    return re.sub(r"[^A-Za-z0-9_-]", "-", raw).rstrip("-")
 
 
 def read_yaml_file(path: str = None) -> dict:

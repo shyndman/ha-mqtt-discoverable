76d5d702f4f44fbd5dedeb43b84e5fe16be48547
Scott Hyndman <scotty.hyndman@gmail.com>
2025-03-06 08:34:59 -0800

    Add additional device class enums from docs, even where entities do not yet exist

--- ha_mqtt_discoverable/sensors.py
+++ ha_mqtt_discoverable/sensors.py
@@ -18,17 +18,17 @@ from __future__ import annotations
 
 import json
 import logging
-from enum import Enum
 from typing import Any, Optional
 
 from pydantic import Field
 
-from ha_mqtt_discoverable import (
+from . import (
     DeviceInfo,
     Discoverable,
     EntityInfo,
     Subscriber,
 )
+from .device_class import BinarySensorDeviceClass, ButtonDeviceClass, CoverDeviceClass, NumberDeviceClass, SensorDeviceClass, SwitchDeviceClass
 
 logger = logging.getLogger(__name__)
 
@@ -96,6 +96,9 @@ class SwitchInfo(EntityInfo):
     """Switch specific information"""
 
     component: str = "switch"
+    device_class: Optional[SwitchDeviceClass] = None
+    """Optional. What type of device this. It will possibly map to google device
+    types."""
     optimistic: Optional[bool] = None
     """Flag that defines if switch works in optimistic mode.
     Default: true if no state_topic defined, else false."""
@@ -154,7 +157,7 @@ class CoverInfo(EntityInfo):
     """Cover specific information"""
 
     component: str = "cover"
-
+    device_class: Optional[CoverDeviceClass] = None
     optimistic: Optional[bool] = None
     """Flag that defines if light works in optimistic mode.
     Default: true if no state_topic defined, else false."""
@@ -189,6 +192,7 @@ class ButtonInfo(EntityInfo):
 
     component: str = "button"
 
+    device_class: Optional[ButtonDeviceClass] = None
     payload_press: str = "PRESS"
     """The payload to send to trigger the button."""
     retain: Optional[bool] = None
@@ -217,7 +221,8 @@ class NumberInfo(EntityInfo):
     """Information about the 'number' entity"""
 
     component: str = "number"
-
+    device_class: Optional[NumberDeviceClass] = None
+    """Optionally specifies what type of entity it is."""
     max: float | int = 100
     """The maximum value of the number (defaults to 100)"""
     min: float | int = 1
@@ -650,182 +655,3 @@ class Select(Subscriber[SelectInfo]):
 
         logger.info(f"Publishing options {opt} to {self._entity.options}")
         self._state_helper(opt)
-
-
-class BinarySensorDeviceClass(Enum, str):
-    """The type of binary sensor."""
-
-    BATTERY = "battery"
-    """On means low, Off means normal."""
-    BATTERY_CHARGING = "battery_charging"
-    """On means charging, Off means not charging."""
-    CO = "co"
-    """On means carbon monoxide detected, Off means no carbon monoxide (clear)."""
-    COLD = "cold"
-    """On means cold, Off means normal."""
-    CONNECTIVITY = "connectivity"
-    """On means connected, Off means disconnected."""
-    DOOR = "door"
-    """On means open, Off means closed."""
-    GARAGE_DOOR = "garage_door"
-    """On means open, Off means closed."""
-    GAS = "gas"
-    """On means gas detected, Off means no gas (clear)."""
-    HEAT = "heat"
-    """On means hot, Off means normal."""
-    LIGHT = "light"
-    """On means light detected, Off means no light."""
-    LOCK = "lock"
-    """On means open (unlocked), Off means closed (locked)."""
-    MOISTURE = "moisture"
-    """On means wet, Off means dry."""
-    MOTION = "motion"
-    """On means motion detected, Off means no motion (clear)."""
-    MOVING = "moving"
-    """On means moving, Off means not moving (stopped)."""
-    OCCUPANCY = "occupancy"
-    """On means occupied, Off means not occupied (clear)."""
-    OPENING = "opening"
-    """On means open, Off means closed."""
-    PLUG = "plug"
-    """On means plugged in, Off means unplugged."""
-    POWER = "power"
-    """On means power detected, Off means no power."""
-    PRESENCE = "presence"
-    """On means home, Off means away."""
-    PROBLEM = "problem"
-    """On means problem detected, Off means no problem (OK)."""
-    RUNNING = "running"
-    """On means running, Off means not running."""
-    SAFETY = "safety"
-    """On means unsafe, Off means safe."""
-    SMOKE = "smoke"
-    """On means smoke detected, Off means no smoke (clear)."""
-    SOUND = "sound"
-    """On means sound detected, Off means no sound (clear)."""
-    TAMPER = "tamper"
-    """On means tampering detected, Off means no tampering (clear)"""
-    UPDATE = "update"
-    """On means update available, Off means up-to-date. The use of this device
-    class should be avoided, please consider using the update entity instead."""
-    VIBRATION = "vibration"
-    """On means vibration detected, Off means no vibration."""
-    WINDOW = "window"
-    """On means open, Off means closed."""
-
-
-class SensorDeviceClass(Enum, str):
-    """Type of sensor."""
-
-    APPARENT_POWER = "apparent_power"
-    """VA	Apparent power"""
-    AQI = "aqi"
-    """None	Air Quality Index"""
-    AREA = "area"
-    """m², cm², km², mm², in², ft², yd², mi², ac, ha	Area"""
-    ATMOSPHERIC_PRESSURE = "atmospheric_pressure"
-    """cbar, bar, hPa, mmHG, inHg, kPa, mbar, Pa, psi	Atmospheric pressure"""
-    BATTERY = "battery"
-    """%	Percentage of battery that is left"""
-    BLOOD_GLUCOSE_CONCENTRATION = "blood_glucose_concentration"
-    """mg/dL, mmol/L	Blood glucose concentration```"""
-    CO2 = "co2"
-    """ppm	Concentration of carbon dioxide."""
-    CO = "co"
-    """ppm	Concentration of carbon monoxide."""
-    CONDUCTIVITY = "conductivity"
-    """S/cm, mS/cm, µS/cm	Conductivity"""
-    CURRENT = "current"
-    """A, mA	Current"""
-    DATA_RATE = "data_rate"
-    """bit/s, kbit/s, Mbit/s, Gbit/s, B/s, kB/s, MB/s, GB/s, KiB/s, MiB/s, GiB/s	Data rate"""
-    DATA_SIZE = "data_size"
-    """bit, kbit, Mbit, Gbit, B, kB, MB, GB, TB, PB, EB, ZB, YB, KiB, MiB, GiB, TiB, PiB, EiB, ZiB, YiB	Data size"""
-    DATE = "date"
-    """	Date. Requires native_value to be a Python datetime.date object, or None."""
-    DISTANCE = "distance"
-    """km, m, cm, mm, mi, nmi, yd, in	Generic distance"""
-    DURATION = "duration"
-    """d, h, min, s, ms	Time period. Should not update only due to time passing. The device or service needs to give a new data point to update."""
-    ENERGY = "energy"
-    """J, kJ, MJ, GJ, mWh, Wh, kWh, MWh, GWh, TWh, cal, kcal, Mcal, Gcal	Energy, this device class should be used for sensors representing energy consumption, for example an electricity meter. Represents power over time. Not to be confused with power."""
-    ENERGY_DISTANCE = "energy_distance"
-    """kWh/100km, mi/kWh, km/kWh	Energy per distance, this device class should be used to represent energy consumption by distance, for example the amount of electric energy consumed by an electric car."""
-    ENERGY_STORAGE = "energy_storage"
-    """J, kJ, MJ, GJ, mWh, Wh, kWh, MWh, GWh, TWh, cal, kcal, Mcal, Gcal	Stored energy, this device class should be used for sensors representing stored energy, for example the amount of electric energy currently stored in a battery or the capacity of a battery. Represents power over time. Not to be confused with power."""
-    ENUM = "enum"
-    """	The sensor has a limited set of (non-numeric) states. The options property must be set to a list of possible states when using this device class."""
-    FREQUENCY = "frequency"
-    """Hz, kHz, MHz, GHz	Frequency"""
-    GAS = "gas"
-    """m³, ft³, CCF	Volume of gas. Gas consumption measured as energy in kWh instead of a volume should be classified as energy."""
-    HUMIDITY = "humidity"
-    """%	Relative humidity"""
-    ILLUMINANCE = "illuminance"
-    """lx	Light level"""
-    IRRADIANCE = "irradiance"
-    """W/m², BTU/(h⋅ft²)	Irradiance"""
-    MOISTURE = "moisture"
-    """%	Moisture"""
-    MONETARY = "monetary"
-    """ISO 4217	Monetary value with a currency."""
-    NITROGEN_DIOXIDE = "nitrogen_dioxide"
-    """µg/m³	Concentration of nitrogen dioxide"""
-    NITROGEN_MONOXIDE = "nitrogen_monoxide"
-    """µg/m³	Concentration of nitrogen monoxide"""
-    NITROUS_OXIDE = "nitrous_oxide"
-    """µg/m³	Concentration of nitrous oxide"""
-    OZONE = "ozone"
-    """µg/m³	Concentration of ozone"""
-    PH = "ph"
-    """None	Potential hydrogen (pH) of an aqueous solution"""
-    PM1 = "pm1"
-    """µg/m³	Concentration of particulate matter less than 1 micrometer"""
-    PM25 = "pm25"
-    """µg/m³	Concentration of particulate matter less than 2.5 micrometers"""
-    PM10 = "pm10"
-    """µg/m³	Concentration of particulate matter less than 10 micrometers"""
-    POWER = "power"
-    """mW, W, kW, MW, GW, TW	Power."""
-    POWER_FACTOR = "power_factor"
-    """%, None	Power Factor"""
-    PRECIPITATION = "precipitation"
-    """cm, in, mm	Accumulated precipitation"""
-    PRECIPITATION_INTENSITY = "precipitation_intensity"
-    """in/d, in/h, mm/d, mm/h	Precipitation intensity"""
-    PRESSURE = "pressure"
-    """cbar, bar, hPa, mmHg, inHg, kPa, mbar, Pa, psi	Pressure."""
-    REACTIVE_POWER = "reactive_power"
-    """var	Reactive power"""
-    SIGNAL_STRENGTH = "signal_strength"
-    """dB, dBm	Signal strength"""
-    SOUND_PRESSURE = "sound_pressure"
-    """dB, dBA	Sound pressure"""
-    SPEED = "speed"
-    """ft/s, in/d, in/h, in/s, km/h, kn, m/s, mph, mm/d, mm/s	Generic speed"""
-    SULPHUR_DIOXIDE = "sulphur_dioxide"
-    """µg/m³	Concentration of sulphure dioxide"""
-    TEMPERATURE = "temperature"
-    """°C, °F, K	Temperature."""
-    TIMESTAMP = "timestamp"
-    """	Timestamp. Requires native_value to return a Python datetime.datetime object, with time zone information, or None."""
-    VOLATILE_ORGANIC_COMPOUNDS = "volatile_organic_compounds"
-    """µg/m³	Concentration of volatile organic compounds"""
-    VOLATILE_ORGANIC_COMPOUNDS_PARTS = "volatile_organic_compounds_parts"
-    """ppm, ppb	Ratio of volatile organic compounds"""
-    VOLTAGE = "voltage"
-    """V, mV, µV, kV, MV	Voltage"""
-    VOLUME = "volume"
-    """L, mL, gal, fl. oz., m³, ft³, CCF	Generic volume, this device class should be used for sensors representing a consumption, for example the amount of fuel consumed by a vehicle."""
-    VOLUME_FLOW_RATE = "volume_flow_rate"
-    """m³/h, ft³/min, L/min, gal/min, mL/s	Volume flow rate, this device class should be used for sensors representing a flow of some volume, for example the amount of water consumed momentarily."""
-    VOLUME_STORAGE = "volume_storage"
-    """L, mL, gal, fl. oz., m³, ft³, CCF	Generic stored volume, this device class should be used for sensors representing a stored volume, for example the amount of fuel in a fuel tank."""
-    WATER = "water"
-    """L, gal, m³, ft³, CCF	Water consumption"""
-    WEIGHT = "weight"
-    """kg, g, mg, µg, oz, lb, st	Generic mass; weight is used instead of mass to fit with every day language."""
-    WIND_DIRECTION = "wind_direction"
-    """°	Wind direction"""
-    WIND_SPEED = "wind_speed"
-    """ft/s, km/h, kn, m/s, mph	Wind speed"""
